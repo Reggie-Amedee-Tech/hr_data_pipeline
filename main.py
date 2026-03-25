92306e4c5bf05740762d70bacc15ced298eb34a8
Reginald Jean Amedee <jeantechnical1992@gmail.com>
2026-03-24 20:30:47 -0400

    added plt.close() to ensure data set isnt being added on top of each other.

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from data_visualizations import data_visualizations
 list_of_phases = os.listdir("./data/")
 
 def run(file: str):
-
-    print(file)
     filename = file[7:]
 
     data = []
@@ -23,13 +21,21 @@ def run(file: str):
     ran = data_analysis.range(cleaned_list)
     var = data_analysis.variance(cleaned_list)
     
-    line_plot = data_visualizations.plotLineChart(cleaned_list)
+    data_visualizations.plotLineChart(cleaned_list)
     plt.savefig(f'./images/{filename}.jpg', dpi=300, bbox_inches="tight")
+    plt.close()
 
     return f'{file} metrics: \n Average: {avg} \n Median: {med} \n Range: {ran} \n Amount of poor data quality captured: {cleaned_list["poor_data_quality_capture"]} \n Variance: {var}'
 
 
 for phase in list_of_phases:
     run(f'./data/{phase}')
+
+# if __name__ == "__main__":
+#     run("data/phase0.txt")
+#     run("data/phase1.txt")
+#     run("data/phase2.txt")
+#     run("data/phase3.txt")
+#     run("data/phase4.txt")
 
 
